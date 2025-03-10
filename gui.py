4a614ea227b02b6a3dd334252e0772c08ce7b868
schromya <schromya@gmail.com>
2025-03-10 14:33:36 -0500

    Gpt starter code is a big gibberish (of course) but decent starting place

--- gui.py
+++ gui.py
@@ -26,9 +26,9 @@ def generate_frames():
                b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
         time.sleep(0.033)  # ~30 FPS
 
-@app.route('/video_feed')
+@app.route('/')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
