a4ca4ee8b8b32a090ec4578fa41b6f2bbdf62489
schromya <schromya@gmail.com>
2025-10-12 16:21:58 -0500

    Added function comments

--- libs/robot_motion_interface/cpp/include/robot_motion_interface/utils/eigen_conversion.hpp
+++ libs/robot_motion_interface/cpp/include/robot_motion_interface/utils/eigen_conversion.hpp
@@ -6,6 +6,13 @@
 
 namespace robot_motion_interface {
 
+/**
+ * @brief Converts a fixed-size std::array to an Eigen::VectorXd
+ * @tparam T Element type (e.g., double, float)
+ * @tparam N Number of elements in the array
+ * @param arr (N) The input std::array.
+ * @return (N) A Eigen Vector copied from arr
+ */
 
 template <typename T, std::size_t N>
 inline Eigen::VectorXd array_to_eigen(const std::array<T, N>& arr)
@@ -15,6 +22,14 @@ inline Eigen::VectorXd array_to_eigen(const std::array<T, N>& arr)
     return vec;
 }
 
+
+/**
+ * @brief Converts an Eigen::VectorXd to a fixed-size std::array
+ * @tparam T Element type (e.g., double, float)
+ * @tparam N Number of elements to copy
+ * @param vec (N) The input Eigen::VectorXd
+ * @return (N) A fixed-size array copied from vec
+ */
 template <typename T, std::size_t N>
 inline std::array<T, N> eigen_to_array(const Eigen::VectorXd& vec)
 {
