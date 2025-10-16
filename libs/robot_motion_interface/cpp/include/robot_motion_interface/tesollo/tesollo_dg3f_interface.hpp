15ccc1ceb2915cf5685c2fa6c7c065f8c4191b89
schromya <schromya@gmail.com>
2025-10-16 17:48:44 -0500

    Realized tesollo requires realtimes (external) controller in cpp so starting that (not tested yet)

---
+++ libs/robot_motion_interface/cpp/include/robot_motion_interface/tesollo/tesollo_dg3f_interface.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "robot_motion_interface/interface.hpp"
+#include "robot_motion_interface/utils/eigen_conversion.hpp"
+
+#include <atomic>
+#include <mutex>
+#include <thread>
+#include <chrono>
+// TODO:REMOVE
+#include <iostream>
+
+#include "robot_motion/controllers/joint_torque_controller.hpp"
+#include "tesollo_communication.hpp"
+#include <franka/robot.h>
+#include <franka/exception.h>
+
+
+namespace robot_motion_interface {
+
+
+class TesolloDg3fInterface : public Interface{
+
+public:
+    /**
+    * @brief Construct the panda motion interface
+    * @param ip IP of the Panda
+    * @param port Port of robot
+    * @param joint_names (n_joints) Names of all the joints
+    * @param kp (n_joints) Proportional gains for controllers
+    * @param kd (n_joints) Derivative gains for controllers
+    * @param device_id Device (slave) ID. Default: 1
+    * @param control_loop_rate Frequency to run control loop (hz). Default: 500 hz.
+    */
+    TesolloInterface(std::string ip, int port,  std::vector<std::string> joint_names,
+    Eigen::VectorXd kp, Eigen::VectorXd kd, int device_id = 1, int control_loop_rate = 500);
+
+    /**
+     * @brief Set the controller's target joint positions for ALL joints (not blocking).
+     * @param q (n_joints,) Desired joint angles in radians
+     */
+    void set_joint_positions(Eigen::VectorXd q) override;
+
+
+    /**
+     * @brief Get the current joint positions and velocities in order of joint_names
+     * @return (n_joints * 2,) Current joint angles in radians and joint velocities in rad/s
+     */
+    Eigen::VectorXd joint_state() override;
+
+    /**
+     * @brief Start the background runtime (e.g. for control loop). This is NOT blocking.
+     */
+    void start_loop() override;
+    
+
+protected:
+
+
+    franka::Robot robot_;
+    std::unique_ptr<robot_motion::Controller> controller_;
+
+    std::atomic<bool> control_loop_running_ =  false;
+    Eigen::VectorXd control_loop_state_{Eigen::VectorXd::Zero(14)};
+    std::mutex control_loop_mutex_;
+    
+
+};
+
+} 
---
+++ libs/robot_motion_interface/cpp/src/tesollo/tesollo_dg3f_interface.cpp
@@ -0,0 +1,45 @@
+// Based off https://github.com/Tesollo-Delto/DELTO_B_ROS2/blob/93e332cf3ff009a3a593b8054308ebe253460325/delto_3f_driver/src/delto_3f_external_driver.cpp
+// This is considered "external" driving of Tesollo with Real Time Control loop 
+
+#include "robot_motion_interface/panda_interface.hpp"
+
+#include <iostream>
+
+namespace robot_motion_interface {
+
+
+TesolloDg3fInterface::TesolloDg3fInterface(std::string ip, int port,  std::vector<std::string> joint_names,
+    Eigen::VectorXd kp, Eigen::VectorXd kd, int device_id, int control_loop_rate) 
+    : robot_(hostname) {
+    
+
+    rp_ = std::make_unique<robot_motion::RobotProperties>(joint_names); // Will not do Coriolis or Gravity Compensation
+    controller_ = std::make_unique<robot_motion::JointTorqueController>(*rp_, kp, kd, false);
+
+    delto_client = std::make_unique<DeltoTCP::Communication>(delto_ip, delto_port);
+    delto_client->connect();
+        
+
+};
+
+
+void TesolloDg3fInterface::set_joint_positions(Eigen::VectorXd q){
+    controller_->set_setpoint(q);
+};
+
+
+
+Eigen::VectorXd TesolloDg3fInterface::joint_state() {
+
+
+};
+
+
+
+void TesolloDg3fInterface::start_loop() {
+    
+
+};
+
+
+} 
