fb01f7ef73320590eb374f5856b4871f2d1f76ab
Jeffrey <jeffrey@theliuhome.com>
2025-10-21 10:48:06 -0500

    added python_wrapper.py and changed file path to .so file to run RangedIK

--- libs/robot_motion/src/robot_motion/ik/ranged_ik.py
+++ libs/robot_motion/src/robot_motion/ik/ranged_ik.py
@@ -1,12 +1,13 @@
 from robot_motion.ik.ik import IK
 import numpy as np
+import python_wrapper as RelaxedIKRust
 
 # TODO: Function comments
 
 class RangedIK(IK):
     def __init__(self):
-        ...
+        print("RangedIK initialized")
     
     def solve(x:np.ndarray, base_frame:str, ee_frame:str) -> np.ndarray:
-        ...
+        print(f"Solving IK for position {x} from {base_frame} to {ee_frame}")
     
