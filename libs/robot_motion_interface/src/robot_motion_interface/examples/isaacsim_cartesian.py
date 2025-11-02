e56c7c567391dd1127a6c714ea38086fadfc9ce0
schromya <schromya@gmail.com>
2025-11-02 13:06:46 -0600

    Increased armature for more stability

--- libs/robot_motion_interface/src/robot_motion_interface/examples/isaacsim_cartesian.py
+++ libs/robot_motion_interface/src/robot_motion_interface/examples/isaacsim_cartesian.py
@@ -14,9 +14,12 @@ def main():
     config_path = config_dir / "isaacsim_config.yaml"
 
     isaac = IsaacsimInterface.from_yaml(config_path)
+    
+    wrist_goal_left = np.array([0, .2, .5, 0, 1.57, 0, 1])
+    wrist_goal_right = np.array([0, .2, .5, 0, -1.57, 0, 1])
 
-    wrist_goal_left = np.array([-0.1, .2, .5, 0.707, 0.707, 0, 0])
-    wrist_goal_right = np.array([0.1, .2, .5, 0.707, 0.707, 0, 0])
+    # wrist_goal_left = np.array([0.0, 0.2, 0.4, 0.707, 0.707, 0, 0])
+    # wrist_goal_right = np.array([0.2, 0.2, 0.4, 0.0,   1.57,  0.0, 1.0])
     
     x = [wrist_goal_left, wrist_goal_right]
     isaac.set_cartesian_pose(x)
@@ -27,3 +30,8 @@ def main():
 if __name__ == "__main__":
    
     main()
+
+
+# Q [ 2.15734012  0.63296362 -1.89509969 -1.97122594  0.61895302  1.76014296 -1.50987322 
+#   -2.01288003 -1.7628      2.16636476 -2.78523487 -2.8973 1.62311001 -0.22353371]
+
