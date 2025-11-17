cbd3ba3ec05a940b7d3e461f9b14c947f24160e8
Jeffrey <jeffrey@theliuhome.com>
2025-11-16 20:24:23 -0600

    cleaned up code and added comments to frame_listener.py

--- libs/robot_description/ros/src/robot_description/frame_listener.py
+++ libs/robot_description/ros/src/robot_description/frame_listener.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
+from geometry_msgs.msg import TransformStamped
 
 
 class TFHelper(Node):
@@ -17,37 +18,39 @@ class TFHelper(Node):
         # self.timer = self.create_timer(1.0, self.print_available_frames)
 
 
-    def lookup_transform(self, source: str, target: str):
-        """Look up transform from source → target frame."""
+    def lookup_transform(self, source: str, target: str) -> TransformStamped:
+        """
+        Lookup the transform from source frame -> target frame.
+
+        Args:
+            source (str): Source frame name
+            target (str): Target frame name
+
+        Returns:
+            TransformStamped: The transform if found, else None
+        """
         try:
             transform = self.tf_buffer.lookup_transform(
                 target,
                 source,
                 rclpy.time.Time()      # time=0 → most recent transform
             )
-            print(1929)
-            print( f"TF lookup successful: {source} → {target}", transform )
+
             return transform
         except TransformException as ex:
             self.get_logger().warn(f"TF lookup failed: {source} → {target}: {ex}")
             return None
 
-    def get_transform(self, source: str, target: str):
-        # now TF buffer is up to date
-        t = self.lookup_transform(source, target)
-
-        # It is important to check if tranform is present or not, as often in the beginning the listener has not yet received any data
-        if t is not None:
-            trans = t.transform.translation
-            rot = t.transform.rotation
-
-            print("Translation:", trans.x, trans.y, trans.z)
-            print("Rotation (quat):", rot.x, rot.y, rot.z, rot.w)
-        else:
-            print("No transform yet!")
-
 
     def print_available_frames(self):
+        """
+        Look for all available TF frames and print them into terminal
+
+        Args:
+            None
+        Returns:
+            None
+        """
         frames = self.tf_buffer.all_frames_as_string()
         frames_yaml = self.tf_buffer.all_frames_as_yaml()
         # self.get_logger().info(f"\nCurrent TF frames:\n{frames}")
@@ -59,9 +62,20 @@ def main():
     node = TFHelper()
 
     # Example usage:
+    source = "table"
+    target = "table_top"
     while rclpy.ok():
         rclpy.spin_once(node)
-        node.get_transform("right_delto_base_link", "table")
+        t = node.lookup_transform(source, target)
+        if t is None:
+            print("Transform not found")
+            continue
+        trans = t.transform.translation
+        rot = t.transform.rotation
+
+        print(f"Transform from {source} to {target}:")
+        print("translation", trans.x, ",", trans.y, ",", trans.z)
+        print("rotation (quat)", rot.x, ",", rot.y, ",", rot.z, ",", rot.w)
 
     rclpy.shutdown()
 if __name__ == '__main__':
