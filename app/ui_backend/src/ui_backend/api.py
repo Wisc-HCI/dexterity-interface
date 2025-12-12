3002780c79bda10cdd07ecdf50e511e9415e68b1
schromya <schromya@gmail.com>
2025-12-12 09:02:34 -0600

    DEMO commit

--- app/ui_backend/src/ui_backend/api.py
+++ app/ui_backend/src/ui_backend/api.py
@@ -177,8 +177,8 @@ def get_current_scene():
 
     # TODO: PASS grasp pose better
     return [
-        {"name": "cup", "description": "Small cup. Height: 0.08 (m). Use this grasp pose: [0.2, 0.11, 0.95, 0, -0.818, 0.574, 0]", "position": [0.2, 0.1, 0.95, 0.0, 0.0, 0.0, 1.0]},
-        {"name": "bowl", "description": "Bowl. Height: 0.0476, Width:0.18 (m). Pour location: [0.22, -0.2, 1.1]", "position": [ 0.2, -0.2, 0.95, 0.0, 0.0, 0.0, 1.0]},
+        {"name": "cup", "description": "Small cup. Height: 0.08 (m). Use this grasp pose: [0.2, 0.11, 1, 0, -0.818, 0.574, 0]", "position": [0.2, 0.1, 0.95, 0.0, 0.0, 0.0, 1.0]},
+        {"name": "bowl", "description": "Bowl. Height: 0.0476, Width:0.18 (m). Pour location: [0.2, -0.2, 1.15, 0, -0.788, -0.614, 0]", "position": [ 0.2, -0.2, 0.95, 0.0, 0.0, 0.0, 1.0]},
     ]
 
 
