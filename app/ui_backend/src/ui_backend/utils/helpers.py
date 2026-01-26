168cd1b908a76ae2ffc36d7a1bc4f1f405e24fe5
schromya <schromya@gmail.com>
2026-01-26 15:00:55 -0600

    Working on many small objects

--- app/ui_backend/src/ui_backend/utils/helpers.py
+++ app/ui_backend/src/ui_backend/utils/helpers.py
@@ -39,18 +39,18 @@ def get_current_scene(all_objects:bool=False):
     primary = [
         {"name": "cup", "description": "Small cup. Height: 0.08 (m). Use this grasp pose: [0.2, 0.11, 1, 0, -0.818, 0.574, 0]", 
             "pose": CUP_POS + [0.0, 0.0, 0.0, 1.0]},
-        {"name": "bowl", "description": "Bowl. Height: 0.0476, Width:0.18 (m). Pour location: [0.2, -0.2, 1.15, 0, -0.788, -0.614, 0]", 
+        {"name": "bowl", "description": "Bowl. Height: 0.0476, Width:0.18 (m). Pour location: [0.2, -0.1, 1.1, 0, -0.788, -0.614, 0]", 
             "pose": [ 0.2, -0.2, 0.95, 0.0, 0.0, 0.0, 1.0]},
     ]
 
     filler  = []
-    
-    for i in range(20):
-        filler.append( 
-            {"name": f"cube_{i}", 
-             "description": "Sugar cube", 
-             "pose": _sample_within_cylinder(CUP_POS,CUP_RADIUS, CUP_HEIGHT)}
-        )
+
+    # for i in range(30):
+    #     filler.append( 
+    #         {"name": f"cube_{i}", 
+    #          "description": "Sugar cube", 
+    #          "pose": _sample_within_cylinder(CUP_POS,CUP_RADIUS, CUP_HEIGHT)}
+    #     )
 
     if all_objects:
         return primary + filler
--- libs/robot_motion_interface/src/robot_motion_interface/isaacsim/config/bimanual_arm_objects_env_config.py
+++ libs/robot_motion_interface/src/robot_motion_interface/isaacsim/config/bimanual_arm_objects_env_config.py
@@ -54,17 +54,6 @@ class BimanualArmObjectSceneCfg(BimanualArmSceneCfg):
         ),
     )
 
-    # cube_1 = RigidObjectCfg(
-    #     prim_path="{ENV_REGEX_NS}/cube_1",
-    #     spawn=sim_utils.CuboidCfg(
-    #         size=(0.08, 0.06, 0.06),
-    #         mass_props = sim_utils.MassPropertiesCfg(mass=0.1),
-    #         rigid_props = sim_utils.RigidBodyPropertiesCfg(rigid_body_enabled=True, kinematic_enabled=False),
-    #         collision_props=sim_utils.CollisionPropertiesCfg(collision_enabled=True),
-    #         visible=False,
-    #     ),
-    # )
-    
     cylinder = RigidObjectCfg(
         prim_path="{ENV_REGEX_NS}/cylinder",
         spawn=sim_utils.CylinderCfg(
@@ -87,12 +76,15 @@ class BimanualArmObjectSceneCfg(BimanualArmSceneCfg):
         ),
     )
 
-
+####################### Many Object Generation ####################### 
 cube_spawn_cfg = sim_utils.CuboidCfg(
             size=(0.01, 0.01, 0.01),
             mass_props = sim_utils.MassPropertiesCfg(mass=0.01),
             rigid_props = sim_utils.RigidBodyPropertiesCfg(rigid_body_enabled=True, kinematic_enabled=False),
             collision_props=sim_utils.CollisionPropertiesCfg(collision_enabled=True),
+            visual_material=sim_utils.PreviewSurfaceCfg(
+                diffuse_color=(0.0, 0.0, 1.0)
+            ),
             visible=False,
         )
 
@@ -110,6 +102,8 @@ for i in range(NUM_CUBES):
             ),
         ),
     )
+
+######################################################################
 
 @configclass
 class BimanualArmObjectEnvCfg(ManagerBasedEnvCfg):
