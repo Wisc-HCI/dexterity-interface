229403c2dd10302c1432f9ce4be6a0e0ee0ec7dd
schromya <schromya@gmail.com>
2026-02-16 18:22:09 -0600

    Tuning a bit more

--- app/ui_backend/src/ui_backend/utils/helpers.py
+++ app/ui_backend/src/ui_backend/utils/helpers.py
@@ -70,13 +70,13 @@ def get_current_scene(all_objects:bool=False):
 
         # TOP grasp
         {"name": "cup", "description": "Small cup", "pose": np.array([0.2, 0.1, 0.9369, 0.0, 0.0, 0.0, 1.0]),
-         "grasp_pose": np.array([0, 0.01, 0.04, 0.707, 0.707, 0, 0]), 
+         "grasp_pose": np.array([0, 0.0, 0.06, 0.707, 0.707, 0, 0]), 
          "dimensions": np.array([0.05, 0.05, 0.08])},
         {"name": "cup_1", "description": "Small cup", "pose": np.array([-0.3, 0.1, 0.9369, 0.0, 0.0, 0.0, 1.0]),
-         "grasp_pose": np.array([0, 0.01, 0.04, 0.707, 0.707, 0, 0]), 
+         "grasp_pose": np.array([0, 0.0, 0.06, 0.707, 0.707, 0, 0]), 
          "dimensions": np.array([0.05, 0.05, 0.08])},
         {"name": "cup_2", "description": "Small cup", "pose": np.array([0.1, 0.0, 0.9369, 0.0, 0.0, 0.0, 1.0]),
-         "grasp_pose": np.array([0, 0.01, 0.04, 0.707, 0.707, 0, 0]), 
+         "grasp_pose": np.array([0, 0.0, 0.06, 0.707, 0.707, 0, 0]), 
          "dimensions": np.array([0.05, 0.05, 0.08])},
 
 
@@ -99,10 +99,10 @@ def get_current_scene(all_objects:bool=False):
 
         # Pincer grasp
         {"name": "bowl", "description": "Bowl.", "pose": np.array([ 0.2, -0.2, 0.9369, 0.0, 0.0, 0.0, 1.0]),
-         "grasp_pose": np.array([0, 0.068, 0.045, 0.707, 0.707, 0, 0]), 
+         "grasp_pose": np.array([0, 0.068, 0.06, 0.707, 0.707, 0, 0]), 
          "dimensions": np.array([0.136, 0.136, 0.0476])},
         {"name": "bowl_1", "description": "Bowl.", "pose": np.array([ -0.3, -0.2, 0.9369, 0.0, 0.0, 0.0, 1.0]),
-         "grasp_pose": np.array([0, 0.068, 0.045, 0.707, 0.707, 0, 0]), 
+         "grasp_pose": np.array([0, 0.068, 0.06, 0.707, 0.707, 0, 0]), 
          "dimensions": np.array([0.136, 0.136, 0.0476])}
     ]
 
