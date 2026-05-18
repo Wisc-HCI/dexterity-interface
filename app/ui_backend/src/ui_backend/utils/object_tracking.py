b83b09411529aae46107e75b550055520cef2855
schromya <schromya@gmail.com>
2026-05-18 14:10:28 -0500

    Added object defaults for task one and two

--- app/ui_backend/src/ui_backend/utils/object_tracking.py
+++ app/ui_backend/src/ui_backend/utils/object_tracking.py
@@ -11,6 +11,62 @@ import numpy as np
 
 _LOGGER = logging.getLogger(__name__)
 
+_TASK_1_DEFAULT_OBJECTS = [
+    {
+        "name": "bowl_1",
+        "description": "Bowl",
+        "pose": np.array([-0.2, 0.15, 0.94, 0.0, 0.0, 0.0, 1.0]),
+        "grasps":{"pincer_grasp":  np.array([-0.068, 0 , 0.065, 1, 0, 0, 0])},
+        "dimensions": np.array([0.136, 0.136, 0.05]),
+        "yolo_labels": ("bowl",),
+    },
+    {
+        "name": "cup_1",
+        "description": "Small cup",
+        "pose": np.array([0.0, 0.15, 0.94, 0.0, 0.0, 0.0, 1.0]),
+        "grasps":{"pincer_grasp":  np.array([0, 0.0, 0.08, 0.707, 0.707, 0, 0])},
+        "dimensions": np.array([0.05, 0.05, 0.08]),
+        "yolo_labels": ("cup", "mug"),
+    },
+    {
+        "name": "spoon_1",
+        "description": "Plastic Spoon",
+        "pose": np.array([0.2, 0.15, 0.94, 0.0, 0.0, 0.707, -0.707]),
+        "grasps":{"pincer_grasp":  np.array([0, 0 , 0.07, 0.707, 0.707, 0, 0])},
+        "dimensions": np.array([0.155, 0.03, 0.01]),
+        "yolo_labels": ("spoon",),
+    },
+
+]
+
+_TASK_2_DEFAULT_OBJECTS = [
+    {
+        "name": "cup_1",
+        "description": "Small cup",
+        "pose": np.array([-0.2, 0.15, 0.94, 0.0, 0.0, 0.0, 1.0]),
+        "grasps":{"pincer_grasp":  np.array([0, 0.0, 0.08, 0.707, 0.707, 0, 0])},
+        "dimensions": np.array([0.05, 0.05, 0.08]),
+        "yolo_labels": ("cup", "mug"),
+    },
+    {
+        "name": "cup_2",
+        "description": "Small cup",
+        "pose": np.array([-0.1, 0.15, 0.94, 0.0, 0.0, 0.0, 1.0]),
+        "grasps":{"pincer_grasp":  np.array([0, 0.0, 0.08, 0.707, 0.707, 0, 0])},
+        "dimensions": np.array([0.05, 0.05, 0.08]),
+        "yolo_labels": ("cup", "mug"),
+    },
+    {
+        "name": "bowl_1",
+        "description": "Bowl",
+        "pose": np.array([0.0, 0.0, 0.94, 0.0, 0.0, 0.0, 1.0]),
+        "grasps":{"pincer_grasp":  np.array([-0.068, 0 , 0.065, 1, 0, 0, 0])},
+        "dimensions": np.array([0.136, 0.136, 0.05]),
+        "yolo_labels": ("bowl",),
+    },
+
+]
+
 
 _TASK_3_STATIONARY_OBJECTS = [{
     "name": "bin_1",
@@ -142,8 +198,11 @@ def _default_scene(task) -> list[dict]:
     Returns:
         (list[dict]): List of default object dictionaries.
     """
-
-    if task == 3:
+    if task == 1:
+        return _TASK_1_DEFAULT_OBJECTS
+    elif task == 2:
+        return _TASK_2_DEFAULT_OBJECTS
+    elif task == 3:
         return _TASK_3_DEFAULT_OBJECTS
     
     return _SCENE_OBJECTS
